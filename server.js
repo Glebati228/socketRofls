a538cb52e704f37e3dfee705f35088efc85b3524
Glebati228 <nub.azazaza@mail.ru>
2020-01-31 22:08:29 +0600

    fixed chat

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 const express = require("express");
 const app = express();
+const fs = require('fs');
+
+// var content = fs.readFileSync('./data/history.txt', 'utf8', function(err, contents)
+// {
+//     if(err)
+//     {
+//         throw err;
+//     }
+
+//     console.log(contents);
+// })
+
+// var data = JSON.parse(content);
+
+var historyContainer = 
+{
+    "chat" : []
+};
 
 app.set('views', __dirname + '/views');
 app.set("view engine", "ejs");
@@ -15,6 +33,22 @@ server = app.listen(process.env.PORT || 3000, function()
 });
 
 const io = require("socket.io")(server);
+
+io.sockets.on('connect', function(socket)
+{
+    var content = fs.readFileSync('./data/history.txt', 'utf8', function(err, contents)
+    {
+        if(err) throw err;
+    });
+
+    if(content)
+    {
+        console.log("load chat");
+        var data = JSON.parse(content);
+        socket.emit('connected', data);
+    }
+});
+
 io.sockets.on("connection", function(socket)
 {
     var name = (socket.id).toString().toUpperCase();
@@ -28,8 +62,19 @@ io.sockets.on("connection", function(socket)
 
     socket.on("send_text", (data) =>
     {
+        historyContainer.chat.push({name : socket.username, text : data.text});
+
+        fs.writeFile('./data/history.txt', JSON.stringify(historyContainer), function(err, data)
+        {
+            if(err) throw err;
+        });
         io.sockets.emit("sended_text", {text : data.text, name: socket.username});
     });
     
     socket.broadcast.emit("userConnected", {name : name});
+
+    socket.on('disconnect', function()
+    {
+
+    });
 });
